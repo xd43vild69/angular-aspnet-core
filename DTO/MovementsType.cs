04cc936345be66689d19f60d49f3defdf798bb18
xd43vild69 <xd43vild69@gmail.com>
2019-04-27 11:47:00 -0500

    DTO layer elements

---
+++ DTO/MovementsType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTO
+{
+    class MovementsType: Entity
+    {
+        public string Name { get; set; }
+    }
+}
